d86ce3efd2f4f50d4569a6e3b74466bf27a0bc4e
aspineli <aspineli@student.42sp.org.br>
2021-04-16 23:46:13 +0000

    succeeded with 100%

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aspineli <aspineli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/16 17:15:23 by aspineli          #+#    #+#             */
+/*   Updated: 2021/04/16 19:39:00 by aspineli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(char **a, char **b)
+{
+	char *c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+}
+
+int		main(int argc, char *argv[])
+{
+	char	*a;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		a = argv[i];
+		while (*a != '\0')
+			write(1, a++, 1);
+		i++;
+		write(1, "\n", 1);
+	}
+}
