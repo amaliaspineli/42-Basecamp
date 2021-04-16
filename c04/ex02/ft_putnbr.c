cbe48f8d62a614caecb096b3e27411ff246f4b01
aspineli <aspineli@student.42sp.org.br>
2021-04-16 23:45:04 +0000

    succeeded with 70%

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aspineli <aspineli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 04:09:11 by aspineli          #+#    #+#             */
+/*   Updated: 2021/04/15 04:13:28 by aspineli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < -0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * (-1));
+	}
+	else
+	{
+		if (nb >= 10)
+			ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
+	}
+}
---
+++ c04/ex03/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int ft_atoi(char *str);
+
+int main(void)
+{
+	char *a;
+
+	a = "   ---784279asfdasdfgsd5639487562";
+
+	printf("Resultado %d\n", ft_atoi(a));
+	return 0;
+}
